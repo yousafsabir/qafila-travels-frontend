4d28864ba6b6214bd676a697979bfe71b504c78d
yousafsabir <myousafdev@gmail.com>
2023-11-14 08:47:08 +0500

    fix: env error from @t3-oss/nextjs-env

--- env.mjs
+++ env.mjs
@@ -1,18 +1,29 @@
-import { createEnv } from '@t3-oss/env-nextjs'
+import { generateErrorMessage } from 'zod-error'
 import { z } from 'zod'
 
-export const env = createEnv({
-	client: {
-		NEXT_PUBLIC_NODE_ENV: z
-			.enum(['development', 'production'])
-			.optional()
-			.default('development'),
-		NEXT_PUBLIC_API_URL: z
-			.string()
-			.regex(
-				new RegExp(
-					/^(http:\/\/|https:\/\/)(localhost:\d+|[\w-]+(\.[\w-]+)+)(:\d+)?(\/.*)?$/,
-				),
-			),
-	},
+const envSchema = z.object({
+	NEXT_PUBLIC_NODE_ENV: z.enum(['development', 'production']).optional().default('development'),
+	NEXT_PUBLIC_API_URL: z.string().url(),
 })
+
+function validatedEnv() {
+	try {
+		const env = {
+			NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+			NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+		}
+		const validationResult = envSchema.parse(env)
+		return validationResult
+	} catch (error) {
+		console.error('Error in Environment Variables')
+		console.error(
+			generateErrorMessage(error.issues, {
+				delimiter: { error: '\n' },
+			}),
+		)
+		// shutting the server down
+		process.exit(1)
+	}
+}
+
+export const env = validatedEnv()
