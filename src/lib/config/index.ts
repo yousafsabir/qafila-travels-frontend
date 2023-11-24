cb15da1fef9769bfae8be95bc5702fe28ab9ee38
yousafsabir <myousafdev@gmail.com>
2023-11-24 11:57:26 +0500

    feat: PAGINATION_LIMIT & NO_VALUE const added earlier

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,3 +1,8 @@
 export * from './env'
 export * from './queryClient'
 export * from './http'
+
+export const PAGINATION_LIMIT = [10, 15, 20, 25, 30]
+
+// "novalue" for manually setting dropdowns to empty
+export const NO_VALUE = "novalue"
