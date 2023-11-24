85a719f07259457df018e1a53e9ed9180667de35
yousafsabir <myousafdev@gmail.com>
2023-11-24 07:32:46 +0500

    feat: added url to store

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -3,12 +3,16 @@ import { User } from '@/lib/interfaces/users'
 
 interface StoreState {
 	admin: User | null
+	url: string | null
 	setAdmin: (data: User | null) => void
+	setUrl: (url: string | null) => void
 }
 
 const useStore = create<StoreState>()((set) => ({
 	admin: null,
+	url: null,
 	setAdmin: (data) => set({ admin: data }),
+	setUrl: (url) => set({ url: url }),
 }))
 
 export default useStore
