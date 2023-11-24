c6af4674549e6f39f6d8162cd9874cb5a7a61ce1
yousafsabir <myousafdev@gmail.com>
2023-11-24 11:53:44 +0500

    feat: pagination interface & common get all response added

--- src/lib/interfaces/http.ts
+++ src/lib/interfaces/http.ts
@@ -7,3 +7,14 @@ export interface HttpCommonResponse {
 	message: string
 	status: number
 }
+
+export interface Pagination {
+	page: number
+	limit: number
+	last_page: number
+	total_count: number
+}
+
+export interface CommonGetAllResponse extends HttpCommonResponse {
+	pagination: Pagination
+}
