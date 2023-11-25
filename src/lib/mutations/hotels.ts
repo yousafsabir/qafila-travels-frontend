10fa3a161e8cfe1f9f4b947077161a2777320ca7
yousafsabir <myousafdev@gmail.com>
2023-11-25 10:40:50 +0500

    refactor: cleanup hotels mutations

--- src/lib/mutations/hotels.ts
+++ src/lib/mutations/hotels.ts
@@ -2,33 +2,8 @@ import toast from 'react-hot-toast'
 import { useMutation, useQuery } from '@tanstack/react-query'
 
 import { queryClient } from '@/lib/config'
-import { httpClient } from '@/lib/config'
 import { getHotels, createHotel, updateHotel } from '@/lib/apis/hotels'
 import { type Hotel, type CreateHotel } from '@/lib/interfaces/hotels'
-
-// export function useLogin() {
-// 	let loadingToast: any
-// 	return useMutation({
-// 		mutationKey: ['admin_login'],
-// 		mutationFn: async (params: UserLogin) => {
-// 			loadingToast = toast.loading('Logging In')
-// 			const res = await userLogin(params)
-// 			return res
-// 		},
-// 		onSuccess: (response) => {
-// 			toast.dismiss(loadingToast)
-// 			if (response.status === 200) {
-// 				toast.success('Logged In')
-// 				// setCookie('access_token', response.access_token, 30)
-// 				httpClient.defaults.headers.common[
-// 					'Authorization'
-// 				] = `Bearer ${response.access_token}`
-// 			} else {
-// 				toast.error(`Error: ${response.message}`)
-// 			}
-// 		},
-// 	})
-// }
 
 export function useCreateHotel() {
 	let loadingToast: any
@@ -80,33 +55,9 @@ export function useUpdateHotel() {
 	})
 }
 
-// export function useGetMe() {
-// 	return useQuery({
-// 		queryKey: ['get_me'],
-// 		queryFn: () => {
-// 			const authToken = getCookie('access_token')
-// 			httpClient.defaults.headers.common['Authorization'] = `Bearer ${authToken}`
-// 			return getMe()
-// 		},
-// 	})
-// }
-
 export function useGetHotels() {
 	return useQuery({
 		queryKey: ['get_hotels'],
 		queryFn: () => getHotels(),
 	})
 }
-
-// export function useLogout() {
-// 	return useMutation({
-// 		mutationKey: ['admin_logout'],
-// 		mutationFn: () => {
-// 			setCookie('access_token', '', 0)
-// 			delete httpClient.defaults.headers.common['Authorization']
-// 			return new Promise((res) =>
-// 				setTimeout(() => res({ message: 'Logged out', status: 200 }), 1000),
-// 			)
-// 		},
-// 	})
-// }
