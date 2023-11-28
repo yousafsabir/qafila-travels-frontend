9222ec14c93ad236b64b409a94518f5b05c4a505
yousafsabir <myousafdev@gmail.com>
2023-11-28 14:09:43 +0500

    feat: common accordion further enhanced

--- src/components/common/CommonAccordion.tsx
+++ src/components/common/CommonAccordion.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import {
 	Accordion,
 	AccordionContent,
@@ -6,17 +7,29 @@ import {
 } from '@/components/common/ui/accordion'
 
 interface IAccordion {
-	label: React.ReactNode
-	content: React.ReactNode
+	className?: string
+	accordions: Array<{
+		label: React.ReactNode
+		content: React.ReactNode
+		wrapperClassName?: string
+		triggerClassName?: string
+		contentClassName?: string
+	}>
 }
 
 export function CommonAccordion(props: IAccordion) {
 	return (
-		<Accordion type='single' collapsible className='w-full'>
-			<AccordionItem value='item-1'>
-				<AccordionTrigger className='flex gap-2'>{props.label}</AccordionTrigger>
-				<AccordionContent>{props.content}</AccordionContent>
-			</AccordionItem>
+		<Accordion type='single' collapsible className={cn('w-full', props.className)}>
+			{props.accordions.map((accordion, i) => (
+				<AccordionItem value='item-1' className={cn(accordion.wrapperClassName)} key={i}>
+					<AccordionTrigger className={cn('flex gap-2', accordion.triggerClassName)}>
+						{accordion.label}
+					</AccordionTrigger>
+					<AccordionContent className={cn(accordion.contentClassName)}>
+						{accordion.content}
+					</AccordionContent>
+				</AccordionItem>
+			))}
 		</Accordion>
 	)
 }
