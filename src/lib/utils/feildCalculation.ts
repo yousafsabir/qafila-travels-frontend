d1f6230e500f902278d00470ceb07a9a79e1a6dd
yousafsabir <myousafdev@gmail.com>
2023-11-29 21:40:54 +0500

    feat: field calculation function added

---
+++ src/lib/utils/feildCalculation.ts
@@ -0,0 +1,148 @@
+import { IFormField } from '@/lib/interfaces'
+
+/**
+ * `arithmeticEvaluate` evaluates arithmetic expressions in the form of strings (e.g. "8 * ( 4 + 5 )")
+ * @param {string} expression
+ * @returns {number}
+ */
+function arithmeticEvaluate(expression: string): number {
+	const operators: Record<string, string[]> = {
+		'+': ['+'],
+		'-': ['-'],
+		'*': ['*', '**'],
+		'/': ['/', '//'],
+	}
+
+	const OPERATIONS: Record<string, (a: number, b: number) => number> = {
+		'+': (a: number, b: number): number => a + b,
+		'-': (a: number, b: number): number => a - b,
+		'*': (a: number, b: number): number => a * b,
+		'/': (a: number, b: number): number => a / b,
+		'**': (a: number, b: number): number => Math.pow(a, b),
+		'//': (a: number, b: number): number => Math.floor(a / b),
+	}
+	// tokenize
+	let i = -1
+	const tokens = []
+	let token = ''
+	while (++i < expression.length) {
+		const ch = expression[i]
+		if (ch == ' ') continue
+
+		if (ch === '(') {
+			let innerExpression = ''
+			let innerBrackets = 0
+			while (expression[++i] !== ')' || innerBrackets !== 0) {
+				innerExpression += expression[i]
+
+				if (expression[i] === '(') innerBrackets++
+				if (expression[i] === ')') innerBrackets--
+
+				if (i >= expression.length) {
+					throw Error('Closing brackets are missing')
+				}
+			}
+
+			if (innerExpression.length) {
+				const value = arithmeticEvaluate(innerExpression)
+				token = String(value)
+			}
+		} else if (operators[ch] && token.length) {
+			const ops = operators[ch]
+			tokens.push(token)
+			token = ''
+			if (ops.length > 1) {
+				// process longer operators
+				let op = ch
+				while (ops.includes(op + expression[i + 1])) {
+					op += expression[++i]
+				}
+				tokens.push(op)
+			} else {
+				tokens.push(ch)
+			}
+		} else {
+			token += ch
+		}
+	}
+	tokens.push(token)
+
+	// calculate
+	let result = 0
+
+	const calculate = (tokens: string[], predicate: (s: string) => boolean) => {
+		i = 0
+		while (i + 1 < tokens.length) {
+			const value1 = tokens[i]
+			const op = tokens[i + 1]
+			const value2 = tokens[i + 2]
+
+			if (predicate(op)) {
+				result = OPERATIONS[op](parseFloat(value1), parseFloat(value2))
+				tokens.splice(i, 3, String(result))
+			} else {
+				i += 2
+			}
+		}
+	}
+
+	// calculate top priority operations
+	calculate(tokens, (op) => op !== '-' && op !== '+')
+	calculate(tokens, (op) => op === '-' || op === '+')
+
+	return result
+}
+
+/**
+ * `replaceKeyWithValue` function inserts values of the keys in a string expression
+ * @param {Record<string, any>} obj
+ * @param {string} expression
+ * @returns {string}
+ */
+function replaceKeyWithValue(obj: Record<string, any>, expression: string): string {
+	Object.entries(obj).forEach(([key, value]) => {
+		if (expression.includes(key) && value) {
+			expression = expression.replace(key, value)
+		}
+	})
+	return expression
+}
+
+/**
+ * `getDependencyArray` function returns all keys in a stringified expression.
+ * For example: for an expression "value_1 * ( value_2 + value_3 )" it will return ["value_1", "value_2", "value_3"]
+ * @param {IFormField<any>[]} fields
+ * @param {expression} expression
+ * @returns {string[]}
+ */
+function getDependencyArray(fields: IFormField<any>[], expression: string): string[] {
+	let returnArr: string[] = []
+	fields.forEach((field) => {
+		if (expression.includes(field.key as string)) {
+			returnArr.push(field.key as string)
+		}
+	})
+	return returnArr
+}
+
+/**
+ * `checkFields` function checks specified fields of an object to truthy. if any of the specified keys has
+ * falsy value, it return false
+ * @param {Record<string, any>} obj
+ * @param {string[] } fields
+ * @returns {boolean}
+ */
+function checkFields(obj: Record<string, any>, fields: string[]): boolean {
+	for (let i = 0; i < fields.length; i++) {
+		let key = fields[i]
+		if (!obj[key]) return false
+	}
+	return true
+}
+
+export const fieldCalculation = {
+	replaceKeyWithValue,
+	getDependencyArray,
+	checkFields,
+	arithmeticEvaluate,
+}
