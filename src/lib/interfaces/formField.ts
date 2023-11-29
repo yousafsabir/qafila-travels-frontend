6d6f70916d35eb7d4bd6958a4b79c046a5456855
yousafsabir <myousafdev@gmail.com>
2023-11-29 21:37:51 +0500

    feat: calculated type added to IFormField & documented it as well

--- src/lib/interfaces/formField.ts
+++ src/lib/interfaces/formField.ts
@@ -1,20 +1,52 @@
 import * as z from 'zod'
 
 type NormalValue = {
+	/**
+	 * @description value that is not calculated from other values in the same object
+	 */
 	valueType: 'normal'
 }
 
 type CalculatedValue = {
+	/**
+	 * @description value that is calculated from other values in the same object
+	 */
 	valueType: 'calculated'
 	calculationType: 'arithmetic' | 'string-interpolated'
-	calculation: string
+	/**
+	 * @description Expression that would be resolved in calculation
+	 * 
+	 * @example
+	 * Arithmetic Expression would be something: "var_1 + var_2 / (var_3 * var_4)"
+	 * While String interpolated one would be something like: "var_1 var_2"
+	 * 
+	 * @see Note: Be careful while specifying variable keys in the expression, because
+	 * they can't be validated in any way. This would cause unexpected results
+	 */
+	expression: string // arithmetic regexp: ^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[+\-*/])(?=.*[ _]).+$
+
 }
 
 type CommonInputData<T> = {
+	/**
+	 * @description text of the label of the input field 
+	 */
 	label: string
+	/**
+	 * @description key, by which the value is going to be stored in an object
+	 */
 	key: keyof T
+	/**
+	 * @description to set an input's default value. mainly used for editing 
+	 */
 	defaultValue: string
+	/**
+	 * @description placeholder of an input
+	 */
 	placeholder: string
+	/**
+	 * @description validation of the field by zod
+	 */
 	validation: z.ZodTypeAny | null
 } & (NormalValue | CalculatedValue)
 
@@ -30,4 +62,7 @@ type ISelectField<T> = CommonInputData<T> & {
 	}[]
 }
 
+/**
+ * @description hello world
+ */
 export type IFormField<T> = ITextField<T> | ISelectField<T>
