ecd3c72b299e3567c2aa0b726c6ff6fd624d940c
yousafsabir <myousafdev@gmail.com>
2023-12-05 12:08:22 +0500

    feat: documented + added capitalization in snakeCaseToNormal utility

--- src/lib/utils/snakeCaseToNormal.ts
+++ src/lib/utils/snakeCaseToNormal.ts
@@ -1,3 +1,28 @@
-export function snakeCaseToNormal(variable: string): string {
-	return variable.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/_/g, ' ')
+type SnakeCaseToNormalOptions = {
+	capitalize: boolean
+}
+
+const defaultOptions: SnakeCaseToNormalOptions = {
+	capitalize: false,
+}
+
+/**
+ * `snakeCaseToNormal()` converts both snake case & camel case strings into normal strings
+ * @param {string} variable
+ * @returns {string}
+ */
+export function snakeCaseToNormal(
+	variable: string,
+	options: SnakeCaseToNormalOptions = defaultOptions,
+): string {
+	let returnVal = variable
+		.replace(/([a-z])([A-Z])/g, '$1 $2') // converts camel case to normal
+		.replace(/_/g, ' ') // converts snake case to normal
+		.trim() // trims leading & trailing white spaces
+	options.capitalize
+		? (returnVal = returnVal.replace(/\b\w/g, function (match) {
+				return match.toUpperCase()
+		  }))
+		: null
+	return returnVal
 }
