ff2d2338d344bbc09f20417b0398fa8864df0d99
yousafsabir <myousafdev@gmail.com>
2023-12-07 18:56:11 +0500

    feat: className prop added

--- src/components/CommonModal.tsx
+++ src/components/CommonModal.tsx
@@ -1,18 +1,22 @@
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { forwardRef } from 'react'
+
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { cn } from '@/lib/utils'
 
 export const CommonModal = forwardRef<
 	HTMLButtonElement,
-	{ closeModal?: (...args: any[]) => void; children: React.ReactNode }
+	{ closeModal?: (...args: any[]) => void; children: React.ReactNode; className?: string }
 >((props, triggerRef) => {
 	return (
 		<>
 			<Dialog>
 				<DialogTrigger ref={triggerRef} />
-				<DialogContent className='w-auto p-0'>{props.children}</DialogContent>
+				<DialogContent className={cn('w-auto p-0', props.className)}>
+					{props.children}
+				</DialogContent>
 			</Dialog>
 		</>
 	)
 })
 
-CommonModal.displayName = "CommonModal"
+CommonModal.displayName = 'CommonModal'
