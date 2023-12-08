620595609cbc939d035adae4c08fcc5d2a12e410
yousafsabir <myousafdev@gmail.com>
2023-12-08 10:53:06 +0500

    feat: set default random string length of 16

--- src/lib/utils/getRandString.ts
+++ src/lib/utils/getRandString.ts
@@ -1,9 +1,9 @@
 /**
- * `genRandString()` gives back random string of alphanumeric characters upto given length 
- * @param {number} len 
- * @returns 
+ * `genRandString()` gives back random string of alphanumeric characters upto given length
+ * @param {number} len default: 16
+ * @returns
  */
-export function genRandString(len: number): string {
+export function genRandString(len: number = 16): string {
 	return Math.random()
 		.toString(36)
 		.substring(2, len + 2)
