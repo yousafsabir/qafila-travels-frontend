fd733f0e75958d62636898f539eaf30116b70c10
yousafsabir <myousafdev@gmail.com>
2023-12-18 22:44:01 +0500

    feat: add no value entry to the validation: 'none' select fields

--- src/lib/utils/getFormFields.ts
+++ src/lib/utils/getFormFields.ts
@@ -1,6 +1,7 @@
 import * as z from 'zod'
 
 import { IFormField } from '@/lib/interfaces'
+import { NO_VALUE } from '@/lib/config'
 
 export function getFormFields<T>(
 	form: Record<keyof Omit<T, '_id'>, IFormField<T>>,
@@ -22,6 +23,14 @@ export function getFormFields<T>(
 				field.validation = field.validation?.optional() as never
 			} else if (options.validation === 'none') {
 				field.validation = z.any()
+				if (field.type === 'select')
+					field.values = [
+						{
+							label: '--Select--',
+							value: NO_VALUE,
+						},
+						...field.values,
+					]
 			}
 		}
 		if (!options.calculation && field.valueType === 'derived') {
