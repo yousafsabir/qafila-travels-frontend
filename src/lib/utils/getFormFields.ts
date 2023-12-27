719035977f959cc1c2865218d1c5f37354ee50ff
yousafsabir <myousafdev@gmail.com>
2023-12-27 10:03:15 +0500

    feat: option to fully customize a field in getFormFields()

--- src/lib/utils/getFormFields.ts
+++ src/lib/utils/getFormFields.ts
@@ -1,12 +1,11 @@
 import * as z from 'zod'
 
-import { IFormField } from '@/lib/interfaces'
 import { NO_VALUE } from '@/lib/config'
-import { defaultValueTypes, type DefaultValueTypes } from '@/lib/interfaces'
+import { defaultValueTypes, type DefaultValueTypes, type IFormField } from '@/lib/interfaces'
 
 export function getFormFields<T>(
 	form: Record<keyof Omit<T, '_id' | 'created_at' | 'updated_at'>, IFormField<T>>,
-	keys: 'all' | (keyof T)[],
+	keys: 'all' | (keyof T | IFormField<T>)[],
 	options: {
 		validation?: 'optional' | 'none'
 		calculation?: boolean
@@ -22,39 +21,43 @@ export function getFormFields<T>(
 		keys = Object.keys(form).filter((key) => !excludedKeys.includes(key)) as (keyof T)[]
 	}
 	return keys.map((key) => {
-		let field = { ...form[key as keyof Omit<T, '_id' | 'created_at' | 'updated_at'>] }
-		if (field.type === 'heading') {
+		if (typeof key === 'string') {
+			let field = { ...form[key as keyof Omit<T, '_id' | 'created_at' | 'updated_at'>] }
+			if (field.type === 'heading') {
+				return field
+			} else {
+				if (options.validation === 'optional') {
+					field.validation = field.validation?.optional() as never
+				} else if (options.validation === 'none') {
+					field.validation = z.any()
+					if (field.type === 'select')
+						field.values = [
+							{
+								label: '--Select--',
+								value: NO_VALUE,
+							},
+							...field.values,
+						]
+				}
+			}
+			if (!options.calculation && field.valueType === 'derived') {
+				// ChanginG Derived Field to Normal
+				// @ts-ignore
+				field.valueType = 'normal'
+				// @ts-ignore
+				delete field.derivationType
+				// @ts-ignore
+				delete field.expression
+			}
+			if (
+				!options.derivedDefaultValue &&
+				defaultValueTypes.includes(field.defaultValue as DefaultValueTypes)
+			) {
+				field.defaultValue = ''
+			}
 			return field
 		} else {
-			if (options.validation === 'optional') {
-				field.validation = field.validation?.optional() as never
-			} else if (options.validation === 'none') {
-				field.validation = z.any()
-				if (field.type === 'select')
-					field.values = [
-						{
-							label: '--Select--',
-							value: NO_VALUE,
-						},
-						...field.values,
-					]
-			}
+			return key as IFormField<T>
 		}
-		if (!options.calculation && field.valueType === 'derived') {
-			// ChanginG Derived Field to Normal
-			// @ts-ignore
-			field.valueType = 'normal'
-			// @ts-ignore
-			delete field.derivationType
-			// @ts-ignore
-			delete field.expression
-		}
-		if (
-			!options.derivedDefaultValue &&
-			defaultValueTypes.includes(field.defaultValue as DefaultValueTypes)
-		) {
-			field.defaultValue = ''
-		}
-		return field
 	})
 }
